e80802e86d8656156aeed22a8e17068c179f580f
Paul W <pawebb@cisco.com>
2018-07-25 15:55:11 -0500

    Update Aging_SRs.py
    added an Aged column script with a number of days value.

--- Aging_SRs.py
+++ Aging_SRs.py
@@ -6,7 +6,7 @@ sheet = 'All'
 
 df = pd.read_excel(io=file_name, sheet_name=sheet)
 
-df.rename(columns={'SR Create Date': 'Create_Date'}, inplace=True)
+df.rename(columns={'SR Create Date': 'Create_Date', 'SR Number': 'SR'}, inplace=True)
 
 tday = dt.date.today()
 tdelta = dt.timedelta(days=30)
@@ -15,8 +15,20 @@ aged = tday - tdelta
 
 df = df.loc[df.Create_Date <= aged, :]
 
+# Sets the SR as the index.
+df = df.set_index('SR', drop = True)
+
+# Created the Age column.
+df.insert(2, 'Age', 0)
+
+# Calculates the days between the Create Date and Today.
+df['Age'] = abs(df['Create_Date'].subtract(tday)).dt.days
+
+# df.head()
+
 df.rename(columns={'Create_Date': 'SR Create Date'}, inplace=True)
 
 writer = pd.ExcelWriter('output_test.xlsx')
 df.to_excel(writer)
+# df.to_excel(writer)
 writer.save()
